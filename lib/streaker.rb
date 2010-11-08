3f10ec1ce6ba337b54020f8a2821cd175c548c07
Ross Paffett <ross@rosspaffett.com>
2010-11-08 12:08:37 -0500

    Add Streaker::Search and bin/streaker-search tool

---
+++ lib/streaker.rb
@@ -0,0 +1,1 @@
+require "streaker/search"
---
+++ lib/streaker/search.rb
@@ -0,0 +1,40 @@
+require "shellwords"
+
+module Streaker
+  class Search
+    attr_reader :options
+    
+    def initialize(query, options={})
+      @query = query
+      @options = { :in => "/" }.merge(options)
+    end
+    
+    def results
+      @results ||= `#{command}`.strip.split("\n")
+    end
+    
+    def command
+      "#{find} #{options[:in]} #{query} -not -type d -and #{types}"
+    end
+    
+    def find
+      `which find`.strip
+    end
+    
+    def query
+      criteria = @query.split(/\s+/).map do |term|
+        term = "*#{term}*"
+        "-iname #{term.shellescape}"
+      end
+      '\( ' + criteria.join(" -a ") + ' \)'
+    end
+    
+    def types
+      criteria = %w(avi m4v mov mp4 mpg mkv).map do |type|
+        type = "*.#{type}"
+        "-iname #{type.shellescape}"
+      end
+      '\( ' + criteria.join(" -o ") + ' \)'
+    end
+  end
+end
